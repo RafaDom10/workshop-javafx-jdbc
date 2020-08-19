daa145b062147dc2196e3ac958a3a8558e2966a2
RafaDom10 <r-domingues@outlook.com>
2020-08-19 14:11:05 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,13 +10,15 @@ import java.io.IOException;
 
 public class Main extends Application {
 
+    private static Scene mainScene;
+
     @Override
     public void start(Stage primaryStage) {
         try {
             ScrollPane scrollPane = FXMLLoader.load((getClass().getResource("/gui/MainView.fxml")));
             scrollPane.setFitToHeight(true);
             scrollPane.setFitToWidth(true);
-            Scene mainScene = new Scene(scrollPane);
+            mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFx Application");
             primaryStage.show();
@@ -24,6 +26,10 @@ public class Main extends Application {
         catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Scene getMainScene() {
+        return mainScene;
     }
 
     public static void main(String[] args) {
